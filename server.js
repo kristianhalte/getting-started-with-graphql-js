425e075913f262de4754725483ceae71d9ecc100
Kristian Larsen <kristian@greatcarrot.com>
2018-05-26 03:14:31 +0800

    made a simple message of the day api with a mutation and a query

--- server.js
+++ server.js
@@ -4,42 +4,26 @@ var { buildSchema } = require('graphql');
 
 // Construct a schema, using GraphQL schema language
 var schema = buildSchema(`
-  type RandomDie {
-    numSides: Int!      # Only used to query the numSides. Can be removed
-    rollOnce: Int!      # Only used to query a single dice roll. Can be removed
-    roll(numRolls: Int!): [Int]
+  type Mutation {
+    setMessage(message: String): String
   }
 
   type Query {
-    getDie(numSides: Int): RandomDie
+    getMessage: String
   }
 `);
 
-// This class implements the RandomDie GraphQL type
-class RandomDie {
-  constructor(numSides) {
-    this.numSides = numSides;
-  }
-
-  rollOnce() {
-    return 1 + Math.floor(Math.random() * this.numSides);
-  }
-
-  roll({numRolls}) {
-    var output = [];
-    for (var i = 0; i < numRolls; i++) {
-      output.push(this.rollOnce());
-    }
-    return output;
-  }
-}
-
 // The root provides the top-level API endpoints
+var fakeDatabase = {};
 var root = {
-  getDie: function ({numSides}) {
-    return new RandomDie(numSides || 6);
+  setMessage: function ({message}) {
+    fakeDatabase.message = message;
+    return message;
+  },
+  getMessage: function () {
+    return fakeDatabase.message;
   }
-}
+};
 
 var app = express();
 app.use('/graphql', graphqlHTTP({
